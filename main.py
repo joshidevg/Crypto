e308dc1c09ce1cd25cb47306c51394eb5b6684af
joshidevg <75678303+joshidevg@users.noreply.github.com>
2023-04-28 16:54:49 +0000

    solved a bug related to miner and added comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+'''Blockchain Based Voting'''
+import json
 from flask import Flask, request, render_template, redirect, url_for
 from backend import Blockchain
-import json
 
 #Web App named app
 app = Flask(__name__)
@@ -15,7 +16,8 @@ voterID_array = {'VOID001': 1, 'VOID002': 1, 'VOID003': 1,
 minerID_array=['MOID001','MOID002','MOID003']
 
 def add_voter_id(voter_id,voter_password):
-    if(voter_id != voter_password):
+    '''function to add voter id'''
+    if voter_id != voter_password:
         return -1 # on webpage,it should show a message that Invalid Voter ID or password
     else:
         if voter_id not in voterID_array:
@@ -25,29 +27,33 @@ def add_voter_id(voter_id,voter_password):
             return 0 #on webpage, it should show that the voterID already exists
 
 def remove_voter_id(voter_id,voter_password):
-    if(voter_id not in voterID_array):
+    '''function to remove voter id (not used)'''
+    if voter_id not in voterID_array:
         return -1 # indicates that voterID is not currently registered in the system
-    elif(voter_id!=voter_password):
-        return 0 # indicates that invalid credentials have been entered 
+    elif voter_id!=voter_password:
+        return 0 # indicates that invalid credentials have been entered
     else:
-        del(voterID_array[voter_id])
+        del voterID_array[voter_id]
         return 1 # indicates that voterID removed successfully from voterID array
 
 def find_voter_id(voter_id):
-    if(voter_id not in voterID_array):
+    '''function to get the status of voter'''
+    if voter_id not in voterID_array:
         return -1 # indicates that voterID is not currently registered in the system
     else:
         return voterID_array[voter_id]  # 0 = voted, 1 = not yet voted
 
 def show_current_voters():
+    '''returns voter dictionary'''
     return voterID_array
 
 def show_current_miners():
+    '''returns the tupple containing all miners'''
     return minerID_array
 
 @app.route('/',methods=['GET','POST'])
 def start():
-    # Home Page
+    '''function to call home page'''
     if request.method=='POST':
         print(request.form["submit"])
         if request.form["submit"] == "vote":
@@ -65,11 +71,12 @@ def start():
 
 @app.route('/voter',methods=['POST','GET'])
 def initial():
-    # Voters Home Page
+    '''login page for voter'''
     if request.method=='POST':
         user=request.form["voterID"]
-        ID_stat = find_voter_id(user)
-        if (ID_stat == 1) and request.form["submit"]=='new_vote' and request.form["confirm"] == "Confirm":
+        idstat = find_voter_id(user)
+        if (idstat == 1) and request.form["submit"]=='new_vote' \
+            and request.form["confirm"] == "Confirm":
             return redirect(url_for("put_vote",name=user))
         else:
             return redirect(url_for("control",User="Voter",ID=user))
@@ -78,21 +85,22 @@ def initial():
 
 
 @app.route('/<User>_failure/<ID>')
-def control(User,ID):
-    # render the failure page
-    Reason = "Unknown Error"
-    ID_stat = find_voter_id(ID)
-    if (ID_stat == -1) and (ID not in minerID_array):
-        Reason = "This "+User+"ID does not exist"
-    elif ID_stat == 0 or (ID not in minerID_array):
-        Reason = "You have already voted once"
-    return render_template("failure.html",User=User,ID=ID, Reason=Reason)
+def control(user,id1):
+    '''function to renger the failure page'''
+    reason = "Unknown Error"
+    idstat = find_voter_id(id1)
+    if idstat == -1 and id1 not in minerID_array:
+        # print("")
+        reason = "This "+user+"ID does not exist"
+    elif idstat == 0 or (id1 not in minerID_array):
+        reason = "You have already voted once"
+    return render_template("failure.html",User=user,ID=id1, reason=reason)
 
 @app.route('/put_vote/<name>',methods=['POST','GET'])
 def put_vote(name):
-    # POLL vote by Voter
-    ID_stat = find_voter_id(name)
-    if request.method=='POST'and (ID_stat == 1):
+    '''function to add vote to the block'''
+    idstat = find_voter_id(name)
+    if request.method=='POST'and (idstat == 1):
         voterID_array[name] = 0
         option=request.form['vote']
         print(blockchain.chain)
@@ -104,6 +112,7 @@ def put_vote(name):
 
 @app.route('/put_vote/<name>/success', methods=['GET','POST'])
 def vote_done(name):
+    '''function to call the success page'''
     if request.method=='POST':
         if request.args.get("Success", False) == "OK":
             # render_template('index.html')
@@ -112,16 +121,13 @@ def vote_done(name):
     else:
         return render_template("success.html", UserID = name)
 
-
-# The process of Mining
-# This takes up the transactions done recently 
-# and put all into a block and append to the chain
 @app.route('/mine/', methods=['GET', 'POST'])
 def mine():
+    '''function to mine the block'''
     last_block = blockchain.last_block
     last_proof = last_block['proof']
     proof = blockchain.proof_of_work(last_proof)
-    
+
     block = blockchain.new_block(proof)
     data = {
         'message': "New Block Mined",
@@ -140,19 +146,25 @@ def mine():
 
 @app.route('/miner_login/', methods=['GET', 'POST'])
 def get_miner():
+    '''miner login page'''
     if request.method=='POST':
         user=request.form["MinerID"]
-        print("user = "+str(user))
-        print(user in minerID_array)
-        if user in minerID_array and request.form["submit"]=='newMine' and request.form["confirm"] == "Confirm":
+        # print("user = "+str(user))
+        # print(user in minerID_array)
+        if user in minerID_array and request.form["submit"]=='newMine' \
+            and request.form["confirm"]=="Confirm":
+            # print(user)
+            # print(user in minerID_array)
             return redirect(url_for("mine"))
         else:
+            # print(user in minerID_array)
             return redirect(url_for("control",User="Miner",ID=user))
     else:
         return render_template('mine.html')
 
 @app.route('/chain/', methods=['GET'])
 def full_chain():
+    '''function to see the full chain'''
     # Displays the whole block chain
     data = {
         'chain': blockchain.chain,
@@ -168,5 +180,5 @@ def full_chain():
 
 
 if __name__ == '__main__':
-    # App starts 
+    # App starts
     app.run(host='localhost', port=5500, debug=True)
